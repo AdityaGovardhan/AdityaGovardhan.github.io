f19e5747f8b8d421e6106e48aedb1ebd7d214e10
Aditya Govardhan <dtgovardhan@gmail.com>
2020-06-01 09:14:22 -0700

    Updates

--- precache-manifest.907e08ae44c714268150113b07649981.js
+++ precache-manifest.907e08ae44c714268150113b07649981.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1f9be63694a245f92b6ae9c21802eb15",
+    "revision": "51108631972fe72bd5e6c3c5c9a48c9e",
     "url": "/index.html"
   },
   {
-    "revision": "8a70c1aa38094fc48dec",
+    "revision": "8440f83418e1788c3b28",
     "url": "/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.33cd2b55.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8a70c1aa38094fc48dec",
-    "url": "/static/js/main.4e25d485.chunk.js"
+    "revision": "8440f83418e1788c3b28",
+    "url": "/static/js/main.b7431587.chunk.js"
   },
   {
     "revision": "4305ad129dcf89844c80",
