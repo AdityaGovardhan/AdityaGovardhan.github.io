57dca9a1009fa3d20d9571b79071f98e543c7b7b
Aditya Govardhan <dtgovardhan@gmail.com>
2020-06-01 08:48:31 -0700

    cleanup, WIP

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h2>
+          This is Aditya Govardhan.
+        </h2>
+        <small style={{color: 'grey', fontSize: '16px'}}>&#9935; site under construction</small>
       </header>
     </div>
   );
