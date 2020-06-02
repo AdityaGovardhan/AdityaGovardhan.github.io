37040456bcba5c54ab856341da2186464a483d43
Aditya Govardhan <dtgovardhan@gmail.com>
2020-06-02 08:28:45 -0700

    added navbar

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-// import logo from './logo.svg';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar bg="dark" variant="dark">
+        {/* <Navbar.Brand href="#home">Aditya Govardhan</Navbar.Brand> */}
+        <Nav className="mr-auto">
+          <Nav.Link href="#about_me">about_me</Nav.Link>
+          <Nav.Link href="#projects">projects</Nav.Link>
+          <Nav.Link href="#resume">resume</Nav.Link>
+        </Nav>
+      </Navbar>
+
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h2>
           This is Aditya Govardhan.
         </h2>
