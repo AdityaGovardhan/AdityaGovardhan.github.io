69788b4dec64bd205511c227ca3772088a2d639c
Aditya Govardhan <dtgovardhan@gmail.com>
2020-06-04 04:35:46 -0700

    navbar corrected

--- app/src/App.js
+++ app/src/App.js
@@ -6,23 +6,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar bg="dark" variant="dark">
+    <>
+      <Navbar style={{ justifyContent: "center"}}>
         {/* <Navbar.Brand href="#home">Aditya Govardhan</Navbar.Brand> */}
-        <Nav className="mr-auto">
-          <Nav.Link href="#about_me">about_me</Nav.Link>
-          <Nav.Link href="#projects">projects</Nav.Link>
-          <Nav.Link href="#resume">resume</Nav.Link>
+        <Nav>
+          <Nav.Link href="#about_me" style={{ color: "white", paddingLeft: "32px", paddingRight: "32px" }}>
+            about_me
+          </Nav.Link>
+          <Nav.Link href="#projects" style={{ color: "white", paddingLeft: "32px", paddingRight: "32px" }}>
+            projects
+          </Nav.Link>
+          <Nav.Link href="#resume" style={{ color: "white", paddingLeft: "32px", paddingRight: "32px" }}>
+            resume
+          </Nav.Link>
+          <Nav.Link href="#contact_me" style={{ color: "white", paddingLeft: "32px", paddingRight: "32px" }}>
+            contact_me
+          </Nav.Link>
         </Nav>
       </Navbar>
 
-      <header className="App-header">
-        <h2>
+      <header className="profile-content">
+        <h2 style={{ color: "white" }}>
           This is Aditya Govardhan.
         </h2>
-        <small style={{color: 'grey', fontSize: '16px'}}>&#9935; site under construction</small>
+        <small style={{ color: 'grey', fontSize: '16px' }}>
+          &#9935; site under construction
+        </small>
       </header>
-    </div>
+    </>
   );
 }
 
